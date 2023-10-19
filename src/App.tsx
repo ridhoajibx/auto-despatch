10f73a01ad710cc084810250539c6216897c788d
Jumakri Ridho Fauzi <RidhoInfinitec@INFINITECs-MacBook-Pro.local>
2023-10-19 13:29:04 +0700

    [ridho] - create env files - wip

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import Routes from "@/routes/Routes";
 
 const App: React.FC = () => {
   // const { isLogin } = useAppSelector(selectAuth);
-
+  // using env in vite
+  const ENV = import.meta.env.VITE_APP_API_URL;
+  console.log(ENV, 'cek-env')
   return (
       <BrowserRouter>
         <Routes isLogin={true} />
